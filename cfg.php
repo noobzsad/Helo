9dd3fd8ee56e6061a2d77f341abe5e07318f53b6
noobzsad <77076175+noobzsad@users.noreply.github.com>
2021-02-26 00:09:28 +0700

    helo
    nuyul helo

---
+++ cfg.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/*-----------------------------------*/
+
+/* data header */
+
+$token = "01cf7b2c9003e727b557f26b16a8c4f9920247b257b142a377e99917f12e4f934663d69938bbbaf7fed0133bed448835b7e6be78f088b996f6b4070ccefa42d2af821cb04f68fd2df02555c5c66bb208bfd39-1.0.0";
+$cookie = "excgd=20210223;store-idc=alisg;store-country-code=id;install_id=6932153871700297473;ttreq=1$2d6719309af92e1aa15620424d3ebc65a2fba8f4;passport_csrf_token=d5f3a28a4b6093d4c3173d8caf2228a8;passport_csrf_token_default=d5f3a28a4b6093d4c3173d8caf2228a8;multi_sids=6932153004623889409%3Acf7b2c9003e727b557f26b16a8c4f992;odin_tt=875a6d50a856a44e19447be46912e49774fd1c1e60fe5167101f47b9858a7193de78ff6199cd388f3c67945d25af14d0d01fb5f938c976171d35b4aac07dcd89;d_ticket=1465d718150a3b7cc9f09126c5ef1ba66ff8e;sid_guard=cf7b2c9003e727b557f26b16a8c4f992%7C1614018186%7C5184000%7CFri%2C+23-Apr-2021+18%3A23%3A06+GMT;uid_tt=b075ef686b2c713b0f5663b044111fbc48599181c5e7b2edbf420de617227b78;uid_tt_ss=b075ef686b2c713b0f5663b044111fbc48599181c5e7b2edbf420de617227b78;sid_tt=cf7b2c9003e727b557f26b16a8c4f992;sessionid=cf7b2c9003e727b557f26b16a8c4f992;sessionid_ss=cf7b2c9003e727b557f26b16a8c4f992;MONITOR_WEB_ID=fc7a839c-0233-4e01-8cc8-0d9aca36f245";
+$ua = "Dalvik/2.1.0 (Linux; U; Android 8.1.0; Redmi 5A MIUI/V11.0.2.0.OCKMIXM) NewsArticle/1.2.1";
+$ticket = "1614081532946";
+
+/* data user profil */
+
+$profil = "user_id=6932153004623889409&manifest_version_code=121&current_region=ID&app_version=1.2.1&device_memory=2g&iid=6932153871700297473&gaid=321df679-230c-4c07-936d-6ca85ea7bd5e&original_channel=referral_apk&channel=referral_apk&app_version_minor=1.2.1.03&device_type=Redmi+5A&language=id&resolution=1280*720&openudid=2008f414983b5ab4&enter_client_ab_vids=1664830&radio=LTE&update_version_code=12103&sys_language=in&login_platform=phone&cdid=ef590363-e262-44ce-8e43-8fd5c545dc96&sys_region=ID&os_api=27&tz_name=Asia%2FJakarta&tz_offset=25200&tt_language=id&dpi=256&brand=Xiaomi&bind_platforms=phone&carrier_region=ID&ac=4G&device_id=6928956383329175041&os=android&mcc_mnc=51001&os_version=8.1.0&version_code=121&hevc_supported=1&carrier_region_v2=510&cold_start=0&release_build=v1.2.1.03+Build+referral_apk_ea9499f_20210218&sim_oper=51001&device_brand=Xiaomi&device_platform=android&sim_region=ID&region=id&aid=3817&ui_language=id";
+
+/* data web balance */
+
+$web = "locale=id&_request_from=web&app_version=1.2.1&original_channel=referral_apk&channel=referral_apk&luckycat_version_code=300067&device_type=Redmi+5A&language=id&resolution=1280*720&enter_client_ab_vids=1664830&radio=LTE&update_version_code=12103&sys_language=in&cdid=ef590363-e262-44ce-8e43-8fd5c545dc96&sys_region=ID&tz_offset=25200&tt_language=id&brand=Xiaomi&ac=4G&device_id=6928956383329175041&mcc_mnc=51001&version_code=121&hevc_supported=1&release_build=v1.2.1.03+Build+referral_apk_ea9499f_20210218&sim_oper=51001&device_platform=android&region=id&aid=3817&manifest_version_code=121&current_region=ID&device_memory=2g&iid=6932153871700297473&gaid=321df679-230c-4c07-936d-6ca85ea7bd5e&app_version_minor=1.2.1.03&openudid=2008f414983b5ab4&login_platform=phone&status_bar_height=25&os_api=27&tz_name=Asia%2FJakarta&dpi=256&bind_platforms=phone&carrier_region=ID&os=android&os_version=8.1.0&carrier_region_v2=510&cold_start=0&app_name=Helo&luckycat_version_name=3.0.0-alpha.67-helo&device_brand=Xiaomi&sim_region=ID&ui_language=id";
+
+/* data menjelajahi aplikasi helo */
+
+$read = "manifest_version_code=121&current_region=ID&app_version=1.2.1&device_memory=2g&iid=6932153871700297473&gaid=321df679-230c-4c07-936d-6ca85ea7bd5e&original_channel=referral_apk&channel=referral_apk&app_version_minor=1.2.1.03&device_type=Redmi+5A&language=id&resolution=1280*720&openudid=2008f414983b5ab4&enter_client_ab_vids=1664830&radio=LTE&update_version_code=12103&sys_language=in&login_platform=phone&cdid=ef590363-e262-44ce-8e43-8fd5c545dc96&sys_region=ID&os_api=27&tz_name=Asia%2FJakarta&tz_offset=25200&tt_language=id&dpi=256&brand=Xiaomi&bind_platforms=phone&carrier_region=ID&ac=4G&device_id=6928956383329175041&os=android&mcc_mnc=51001&os_version=8.1.0&version_code=121&hevc_supported=1&carrier_region_v2=510&cold_start=0&release_build=v1.2.1.03+Build+referral_apk_ea9499f_20210218&sim_oper=51001&device_brand=Xiaomi&device_platform=android&sim_region=ID&region=id&aid=3817&ui_language=id&app_name=Helo&bounce_disable=1&luckycat_version_name=3.0.0-alpha.67-helo&luckycat_version_code=300067&status_bar_height=25";
+
+/* data membuka peti */
+
+$open = "locale=id&_request_from=web&manifest_version_code=121&current_region=ID&app_version=1.2.1&device_memory=2g&iid=6932153871700297473&gaid=321df679-230c-4c07-936d-6ca85ea7bd5e&original_channel=referral_apk&channel=referral_apk&app_version_minor=1.2.1.03&device_type=Redmi+5A&language=id&resolution=1280*720&openudid=2008f414983b5ab4&enter_client_ab_vids=1664830&radio=LTE&update_version_code=12103&sys_language=in&login_platform=phone&cdid=ef590363-e262-44ce-8e43-8fd5c545dc96&sys_region=ID&os_api=27&tz_name=Asia%2FJakarta&tz_offset=25200&tt_language=id&dpi=256&brand=Xiaomi&bind_platforms=phone&carrier_region=ID&ac=4G&device_id=6928956383329175041&os=android&mcc_mnc=51001&os_version=8.1.0&version_code=121&hevc_supported=1&carrier_region_v2=510&cold_start=0&release_build=v1.2.1.03+Build+referral_apk_ea9499f_20210218&sim_oper=51001&device_brand=Xiaomi&device_platform=android&sim_region=ID&region=id&aid=3817&ui_language=id&app_name=Helo&bounce_disable=1&luckycat_version_name=3.0.0-alpha.67-helo&luckycat_version_code=300067&status_bar_height=25";
+
+/*-----------------------------------*/
+
+?>
